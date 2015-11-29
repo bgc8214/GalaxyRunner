aee984a6ceef1d95c37fde6b8752b375aad09330
bgc8214 <bgc8214@gmail.com>
2015-11-29 17:48:06 +0900

    test

---
+++ RandomRotater.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RandomRotater : MonoBehaviour
+{
+	float tumble = 5;	
+
+	void Start () 
+	{
+		GetComponent<Rigidbody> ().angularVelocity = Random.insideUnitSphere * tumble;
+	}
+	
+
+}
