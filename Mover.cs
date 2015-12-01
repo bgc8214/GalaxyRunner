6a9313ccc4adefc60ee47d94595c73884cfed967
bgc8214 <bgc8214@gmail.com>
2015-12-01 19:18:05 +0900

    Revert "hpbar 변경"
    This reverts commit f9bddac93deb2307b99112cea4324f55b910d766.

---
+++ Mover.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Mover : MonoBehaviour
+{
+	    public float speed = 5;
+
+	    void Start () 
+	    {
+        
+            GetComponent<Rigidbody> ().velocity = Vector3.forward * speed;
+      
+                
+	    }
+
+
+}
+		
